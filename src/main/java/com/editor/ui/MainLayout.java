df8914443c7f36859f69873fff84d253a0fd3498
Sa-sketch-max <sahilsha635@gmail.com>
2026-07-30 00:07:53 +0530

    feat: create MainLayout

--- src/main/java/com/editor/ui/MainLayout.java
+++ src/main/java/com/editor/ui/MainLayout.java
@@ -1,4 +1,45 @@
 package com.editor.ui;
 
+import javafx.scene.Parent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.SplitPane;
+
 public class MainLayout {
+
+    private final BorderPane root;
+
+    public MainLayout() {
+
+        root = new BorderPane();
+
+        // Top
+        root.setTop(MenuBarBuilder.build());
+
+        // Left
+        root.setLeft(new Sidebar());
+
+        // Center
+        SplitPane splitPane = new SplitPane();
+
+        TextArea editor = new TextArea();
+        editor.setPromptText("Start writing...");
+
+        Label preview = new Label("Markdown Preview");
+
+        splitPane.getItems().addAll(editor, preview);
+
+        splitPane.setDividerPositions(0.6);
+
+        root.setCenter(splitPane);
+
+        // Bottom
+        root.setBottom(new StatusBar());
+    }
+
+    public Parent getRoot() {
+        return root;
+    }
+
 }
